3687118974dada33d442326702d782033fee2981
Ben Mueller <benjm2002@gmail.com>
2022-06-29 13:59:22 -0500

    [Commit time: 6/29/2022, 1:59:22 PM]

--- index.py
+++ index.py
@@ -1,30 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_restful import Api, Resource
+from flask import jsonify
+
+from PIL import Image
+import os
 # import the model here
 
 app = Flask(__name__)
 api = Api(app)
 
+# UPLOAD_FOLDER = '/uploaded_images'
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    # maybe add code to get some info about the model here? 
+    # So that I don't have to edit the html file if I decide to change the model
+    return render_template("about.html")
+
+
 @app.route("/ben")
 def ben():
     return render_template("ben.html")
 
-# add an api endpoint, this is where submitted image is sent
-class ImageClassificationEndpoint(Resource):
-    def post(self):
-        print('testy')
-        # TODO, use the model we have trained to clasify the data
-        # return a JSON object that contains the classification results
-        pass
 
-api.add_resource(ImageClassificationEndpoint, "/classifyImage")
-
-
+@app.route("/classifyImage", methods=['POST'])
+def classify():
+    # TODO create model, then use it to classify image
+    img_file = request.files['image']
+    img = Image.open(img_file)
+    # img.show()
+    prediction="no clue :("
+    return render_template("prediction.html", prediction=prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -1,3 +1,21 @@
 // here we will need to make a POST request via fetch
 // we will send in the submitted image, and get back the classification results
 // we should then display the classificaiton results to the user
+
+// add the listener to the button w the value predict
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     predictBtn = document.querySelector('#predict_button')
+//     imgInput = document.querySelector('#image_file')
+
+
+//     predictBtn.addEventListener("click", () => {
+//         console.log('did it work')
+//         if (imgInput.value === "") {
+//             alert("Please submit an image.")
+//         }
+//         else {
+//             console.log(imgInput.value)
+//         }
+//     })
+// })

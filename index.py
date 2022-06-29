e42ff6ca83d3e6f90eca1e39f5ec276b57704704
Ben Mueller <benjm2002@gmail.com>
2022-06-29 10:01:19 -0500

    [Commit time: 6/29/2022, 10:01:19 AM]

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
-print('checking that codeHistories is working')
+from flask import Flask, render_template
+# import the model here
+
+app = Flask(__name__)
+
+@app.route("")
+def index():
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

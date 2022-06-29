2645162f9ebd3bf45dcef058b978ce2d4fbd699a
Ben Mueller <benjm2002@gmail.com>
2022-06-29 10:09:00 -0500

    [Commit time: 6/29/2022, 10:09:00 AM]

--- index.py
+++ index.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template
+from flask_restful import Api, Resource
 # import the model here
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("")
+
+@app.route("/")
 def index():
-    
+    return render_template("index.html")
 
 
+# add an api endpoint, this is where submitted image is sent
+class ImageClassificationEndpoint(Resource):
+    def post(self):
+        # TODO, use the model we have trained to clasify the data
+        # return a JSON object that contains the classification results
+        pass
 
-
+api.add_resource(ImageClassificationEndpoint, "/classifyImage")
 
 
 
---
+++ static/index.js
@@ -0,0 +1,3 @@
+// here we will need to make a POST request via fetch
+// we will send in the submitted image, and get back the classification results
+// we should then display the classificaiton results to the user
